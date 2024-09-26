39ac6f30636d169acb648aeea2c97dda3a809d2b
abdullahkashif09 <abdullahkashif4611@gmail.com>
2024-09-26 21:26:06 +0500

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from 'react'; // Removed the duplicate import
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Popular from './components/Popular/Popular';
 import Offers from './components/Offers/Offers';
 import NewCollection from './components/NewCollection/NewCollection';
-import NewLetter from './components/NewLetter/NewLetter'
-
+import NewLetter from './components/NewLetter/NewLetter';
 import Footer from './components/Footer/Footer';
-import Shop from './pages/Shop';
+import Shop from './pages/shop';
 import ShopCategory from './pages/ShopCategory';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import LoginSignup from './pages/LoginSignup';
-import men_banner from './components/Assets/banner_mens.png'
-import women_banner from './components/Assets/banner_women.png'
-import kid_banner from './components/Assets/banner_kids.png'
-
+import men_banner from './components/Assets/banner_mens.png';
+import women_banner from './components/Assets/banner_women.png';
+import kid_banner from './components/Assets/banner_kids.png';
 
 const App = () => {
   return (
@@ -26,20 +24,19 @@ const App = () => {
         <Hero />
         <Popular />
         <Offers />
-        <NewCollection/>
-        <NewLetter/>
-      
+        <NewCollection />
+        <NewLetter />
         
         <Routes>
           <Route path='/shop' element={<Shop />} />
           <Route path='/mens' element={<ShopCategory banner={men_banner} category='mens' />} />
           <Route path='/womens' element={<ShopCategory banner={women_banner} category='womens' />} />
-          <Route path='/kids' element={<ShopCategory  banner={kid_banner} category='kids' />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category='kids' />} />
           <Route path='/product/:productId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/loginsignup' element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Descriptionbox.jsx/Descriptionbox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const Descriptionbox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='desciptionbox-navigator'>
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Review (122)</div>
+
+        </div>
+        <div className='descriptionbox-description'>
+            <p>
+            An ecommerce platform is the content management system (CMS) and commerce engine websites use to manage catalogued products, register purchases and manage a users relationship with an online retailer. It doesn't matter if your business is large or small, B2B or B2C, selling tangible goods or providing remote services.</p>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Descriptionbox
