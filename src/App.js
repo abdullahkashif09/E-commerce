bd8ed87a8a3d349cd811aaef500770ebb41c722e
abdullahkashif09 <abdullahkashif4611@gmail.com>
2024-09-27 20:25:37 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'; // Removed the duplicate import
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Popular from './components/Popular/Popular';
-import Offers from './components/Offers/Offers';
-import NewCollection from './components/NewCollection/NewCollection';
-import NewLetter from './components/NewLetter/NewLetter';
 
 import Footer from './components/Footer/Footer'
 import Shop from './pages/Shop'
@@ -22,12 +17,6 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Navbar />
-        <Hero />
-        <Popular />
-        <Offers />
-        <NewCollection />
-        <NewLetter />
-        
         
         <Routes>
           <Route path='/shop' element={<Shop />} />
